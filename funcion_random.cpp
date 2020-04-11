9f0b84c1c70b72f02dee53ce79949f28135a22e5
ruben-mancilla <rmancilla465@gmail.com>
2020-04-10 23:39:34 -0400

    Importación e implementación de File.cpp con ROOT C++

--- funcion_random.cpp
+++ funcion_random.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+//#include 'TROOT.h'
+//#include 'T.TH1F'
 using namespace std;
 
 int raan()
 {
     return rand() % 101;
 }
+
+//Implementación de ROOT C++
+TH1F f4("f4","example histogram",100,0,100);
